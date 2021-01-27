306cc0c604e3f637073ce5a2ec4c8b10d673dab0
ahtrahdis7 <sidharthabiki@gmail.com>
2021-01-28 00:43:58 +0530

    added changes

--- src/screens/main.js
+++ src/screens/main.js
@@ -13,6 +13,58 @@ import Projects from '../components/Projects';
 // import Social from '../components/Social';
 // import CodeIcon from '@material-ui/icons/Code';
 
+const NavBar = () => {
+    return(
+        <div>
+            <AppBar position='fixed'>
+                <Toolbar style={{
+                    backgroundColor: 'white'
+                }}>
+                    <Container style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignContent: 'space-between'
+                    }}>
+                        <Typography variant="h5" style={{
+                            flexGrow: 1,
+                            color: 'black',
+                            fontFamily: 'ComicNeue'
+                        }}>Ahtrahdis7</Typography>
+                        <div style={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems:  'flex-end'
+                        }}>
+                            <a href="#experience" style={{
+                                color: 'white'
+                            }}>
+                            <Typography variant="h6" style={{
+                                flexGrow: 1,
+                                color: 'black',
+                                fontFamily: 'ComicNeue',
+                                marginLeft: 10,
+                                marginRight: 10,
+                            }}> Work</Typography>
+                            </a>
+                            <a href="#projects" style={{
+                                color: 'white'
+                            }}>
+                            <Typography variant="h6" style={{
+                                flexGrow: 1,
+                                color: 'black',
+                                fontFamily: 'ComicNeue',
+                                marginLeft: 10,
+                                marginRight: 10
+                            }}>Projects</Typography>
+                            </a>
+                        </div>
+                    </Container>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
+
 class Main extends React.Component{
 
     render(){
@@ -22,51 +74,7 @@ class Main extends React.Component{
                 <div style={{
                     flexGrow: 1
                 }}>
-                    <AppBar position='fixed'>
-                        <Toolbar style={{
-                            backgroundColor: 'white'
-                        }}>
-                            <Container style={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                alignContent: 'space-between'
-                            }}>
-                                <Typography variant="h5" style={{
-                                    flexGrow: 1,
-                                    color: 'black',
-                                    fontFamily: 'ComicNeue'
-                                }}>Ahtrahdis7</Typography>
-                                <div style={{
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                    alignItems:  'flex-end'
-                                }}>
-                                    <a href="#experience" style={{
-                                        color: 'white'
-                                    }}>
-                                    <Typography variant="h6" style={{
-                                        flexGrow: 1,
-                                        color: 'black',
-                                        fontFamily: 'ComicNeue',
-                                        marginLeft: 10,
-                                        marginRight: 10,
-                                    }}> Experience</Typography>
-                                    </a>
-                                    <a href="#projects" style={{
-                                        color: 'white'
-                                    }}>
-                                    <Typography variant="h6" style={{
-                                        flexGrow: 1,
-                                        color: 'black',
-                                        fontFamily: 'ComicNeue',
-                                        marginLeft: 10,
-                                        marginRight: 10
-                                    }}>Projects</Typography>
-                                    </a>
-                                </div>
-                            </Container>
-                        </Toolbar>
-                    </AppBar>
+                    <NavBar />
                 </div>
                 <Divider />
                 <div id="about">
